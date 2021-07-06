9e9bf335e96aeebd4e1fcd912ed535cfeae1a481
GB14-code <gabi.balcan@yahoo.com>
2021-07-06 11:46:45 +0300

    StringArray

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] strings = new String[]{"PrimulString", "AlDoileaString", "AlTreileaString"};
+
+        StringBuilder addedString = new StringBuilder();
+
+        for (String string : strings) {
+            addedString.append(string);
+        }
+        System.out.println(addedString);
+    }
+}
